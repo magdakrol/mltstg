45f8a017aa0c9152d2e3b7db35a84c9600fe01b4
magdakrol <krolmgd@gmail.com>
2018-08-12 16:41:17 +0200

    prawie wszystko, do szlifu, dodac podstrony

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,40 @@
 document.getElementById('menuicon').addEventListener('click', function(){
     document.getElementById('menu').classList.toggle('open');
 });
+
+$(document).on('ready', function () {
+    $(".multipe-items").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+              breakpoint: 1150,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              },
+        ]
+    });
+
+    $('.variable-width').slick({
+        autoplay: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+        
+      });
+      
+});
+
